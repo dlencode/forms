01757abc23844bb8f3575cf8b757a7257c2597c7
Danila Ermolenko <mail@dlencode.com>
2017-05-14 19:29:15 +0300

    First stable version with smooth animations

--- src/forms.js
+++ src/forms.js
@@ -1,31 +1,55 @@
 class Form {
-  constructor(callSelector, formContainer, formClose) {
+  constructor(callSelector, formContainer, formClose, durationAnimation) {
     this.callSelector = document.querySelectorAll(callSelector);
     this.formContainer = document.querySelector(formContainer);
     this.formClose = document.querySelector(formClose);
+    this.durationAnimation = durationAnimation;
   }
 
   log(callSelector, formContainer) {
     console.log(this.formContainer);
   }
 
-  click(callSelector, formContainer) {
+  click(callSelector, formContainer, durationAnimation) {
     for (let i = 0; i < this.callSelector.length; i++) {
       this.callSelector[i].addEventListener('click', (event) => {
         event.preventDefault();
 
         this.formContainer.style.display = 'flex';
         setTimeout(() => {
-          var test = anime({
+          anime({
             targets: this.formContainer,
             opacity: 1,
-            easing: 'linear'
           });
-          let fadeInFormContent = anime({
+          anime({
             targets: '.formjs__content',
             opacity: 1,
-            translateY: -100,
-            delay: 250
+            translateY: [100, 0],
+            delay: 100,
+            duration: this.durationAnimation
+          });
+          anime({
+            targets: '.formjs__content--header',
+            opacity: [0, '1'],
+            translateY: [50, 0],
+            duration: this.durationAnimation,
+            delay: 200
+          });
+          anime({
+            targets: '.formjs__input',
+            opacity: [0, '1'],
+            translateY: [50, 0],
+            duration: this.durationAnimation,
+            delay: function(el, index) {
+              return index * 250 + 300;
+            }
+          });
+          anime({
+            targets: '.formjs__footer--send',
+            opacity: [0, '1'],
+            translateY: [50, 0],
+            duration: this.durationAnimation,
+            delay: 700
           });
         }, 250);
       })
@@ -36,11 +60,12 @@ class Form {
     this.formClose.addEventListener('click', () => {
       // this.formContainer.style.opacity = 0;
 
-      let fadeOutFormContent = anime({
+      anime({
         targets: '.formjs__content',
         opacity: 0,
-        translateY: 200,
-        delay: 0
+        translateY: [0, 100],
+        delay: 250,
+        duration: 2000
       });
 
       setTimeout(() => {
@@ -63,7 +88,7 @@ class Libs {
   }
 }
 
-let test = new Form('.formjs__call', '.formjs', '.formjs__close-area');
+let test = new Form('.formjs__call', '.formjs', '.formjs__close-area', 2000);
 // test.log();
 test.click();
 test.close();
